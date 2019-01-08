4a23037f7267a694247c91cec6ffba22d25fad53
Dawid Kalicki <dawod28@gmail.com>
2019-01-08 22:25:27 +0100

    small rofi-menu fixes

---
+++ polybar/settings.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+POWER="\t\t WiFi\n\t\t Bluetooth\n\t_\tAudio"
+ROFI=`echo -e $POWER | rofi -dmenu -lines 4 -columns 1 -location 7 -yoffset -45 -p "      SETTINGS" -width 25 -hide-scrollbar | awk '{print $1}'`
+
+if [ ${#ROFI} -gt 0 ]
+then
+    case $ROFI in
+    )
+        CMD="connman-gtk"
+        ;;
+    )
+        CMD="blueberry"
+        ;;
+    _)
+        CMD="pavucontrol"
+        ;;
+    esac
+fi
+
+$CMD
+
+# add font settings ?
